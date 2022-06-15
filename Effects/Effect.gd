df7e6efbe871b984e8cd7278ae6bc4065bb95ea5
QZY <1598435288@qq.com>
2022-06-15 22:38:01 +0800

    first commit

---
+++ Effects/Effect.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite2D
+
+
+func _ready():
+	self.connect("animation_finished", Callable(self,"_on_animation_finished") )
+	frame=0
+	play("Animate")
+
+
+
+func _on_animation_finished():
+	queue_free()

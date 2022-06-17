fc952eba2dce7f9da2ab278aec527e4829aecd08
QZY <1598435288@qq.com>
2022-06-17 12:20:59 +0800

    实例化大部分角色并实现了另一可操纵角色

--- tijao.ps1
+++ tijao.ps1
@@ -2,7 +2,7 @@ cls
 
 git add .
 git commit -m "实例化大部分角色并实现了另一可操纵角色"
-git branch -M main
+git branch -M QZY
 git remote add origin https://github.com/netdeleishui/base-on-godot.git
 git push -u origin main
 pause
